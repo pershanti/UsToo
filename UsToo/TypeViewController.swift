7145381cf4710f7709724cafe014bbbe762b48fb
pershanti <shantini29@gmail.com>
2017-10-21 13:28:13 -0700

    connecting view controllers

---
+++ UsToo/TypeViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TypeViewController.swift
+//  UsToo
+//
+//  Created by Shantini Vyas on 10/21/17.
+//  Copyright © 2017 Shantini Vyas. All rights reserved.
+//
+
+import UIKit
+
+class TypeViewController: UITableViewController {
+    
+    weak var delegate: FormViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+
+}
---
+++ UsToo/TypeViewControllerDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  TypeViewControllerDelegate.swift
+//  UsToo
+//
+//  Created by Shantini Vyas on 10/21/17.
+//  Copyright © 2017 Shantini Vyas. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+protocol TypeViewControllerDelegate: class {
+    func doneButton(_ by: UITableViewController, assaultList: [String])
+}
